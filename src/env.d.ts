3744fce5cde565218c6a2e885ee12867409cd739
elebirds <hhmcn@outlook.com>
2026-03-23 04:24:37 +0800

    feat: add content source mode contract

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,8 @@
 /// <reference types="astro/client" />
 /// <reference path="../.astro/types.d.ts" />
+
+interface ImportMetaEnv {
+	readonly DUET_CONTENT_LOCAL_PATH?: string;
+	readonly DUET_CONTENT_REPO?: string;
+	readonly CONTENT_REPO_TOKEN?: string;
+}
